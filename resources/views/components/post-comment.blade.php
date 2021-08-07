00eb160f166f46d31a901d80e648e606cc4e954f
droll552 <droll552@mail.ru>
2021-08-07 21:52:02 +0300

    added custom auth and hardcoded comments

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,17 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.privatar.cc//100" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted 8 months ago
+            </p>
+        </header>
+        <p>
+            comment text
+        </p>
+    </div>
+</article>
